e8dfcbe0e7002ffb56d7df873eb83ef382808590
steve whitehouse <stewhites@blueyonder.co.uk>
2024-02-28 09:38:43 +0000

    add comment feature POSt and feedback

--- codestar/settings.py
+++ codestar/settings.py
@@ -48,14 +48,19 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
+    'blog',
     'about',
     ]
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
